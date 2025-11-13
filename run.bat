3cfa1ec625f6cfe729d7d84aa8742474fb34492a
gianky00 <62794270+gianky00@users.noreply.github.com>
2025-11-13 21:05:06 +0100

    add

--- run.bat
+++ run.bat
@@ -1,7 +1,11 @@
 @echo off
+echo Ensuring pip is installed...
+py -m ensurepip --upgrade
+
 echo Installing dependencies...
-pip install -r requirements.txt
+py -m pip install -r requirements.txt
 
 echo Starting the application...
 set PYTHONPATH=.
-python desktop_app/main_window.py
+py desktop_app/main_window.py
+pause
