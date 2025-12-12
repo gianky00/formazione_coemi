edb3c2dbddabc894e74da5cfd218aeb4d81711e7
00gianky00 <lavoro.allegretti@gmail.com>
2025-12-12 08:16:16 +0100

    add

--- desktop_app/core/__init__.py
+++ desktop_app/core/__init__.py
@@ -2,16 +2,25 @@
 """
 Core module per desktop_app.
 Contiene componenti fondamentali per widget lifecycle protection.
+Contiene componenti fondamentali per widget lifecycle protection e animation management.
 """
 
 from .widget_guard import (
     is_widget_alive,
-    is_qobject_alive,
-    guard_widget_access,
-    guard_widget_access_with_fallback,
-    WidgetRef,
     safe_widget_context,
     WidgetGuardian,
+)
+
+from .animation_manager import (
+    animation_manager,
+    AnimationManager,
+    fade_in,
+    fade_out,
+    slide_in,
+    slide_out,
+    animate_property,
+    shake_widget,
+    animate_geometry,
 )
 
 __all__ = [
@@ -22,4 +31,13 @@ __all__ = [
     'WidgetRef',
     'safe_widget_context',
     'WidgetGuardian',
+    'animation_manager',
+    'AnimationManager',
+    'fade_in',
+    'fade_out',
+    'slide_in',
+    'slide_out',
+    'animate_property',
+    'shake_widget',
+    'animate_geometry',
 ]
